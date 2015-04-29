9212804dcfb320335d3b3412404c1856b06a2b65
Julien Letrouit <jletrouit@mnubo.com>
2015-04-29 16:38:06 -0400

    MM-2023: code structure improvements.

---
+++ tool/src/main/scala/com/mnubo/dbschemas/DbMigrationConfig.scala
@@ -0,0 +1,49 @@
+package com.mnubo.dbschemas
+
+import com.typesafe.config.Config
+
+case class DbMigrationConfig(db: Database,
+                             schemaName: String,
+                             host: String,
+                             port: Int,
+                             username: String,
+                             password: String,
+                             name: String,
+                             createDatabaseStatement: String,
+                             drop: Boolean,
+                             version: Option[String],
+                             wholeConfig: Config)
+
+case class DbSchemasArgsConfig(drop: Boolean = false,
+                               version: Option[String] = None,
+                               cmd: DbCommand = Migrate)
+
+object DbMigrationConfig {
+  def apply(args: DbSchemasArgsConfig, config: Config, namespace: Option[String]): DbMigrationConfig = {
+    val schemaName =
+      config.getString("schema_name")
+
+    val nameProvider =
+      getClass
+        .getClassLoader
+        .loadClass(config.getString("name_provider_class"))
+        .newInstance()
+        .asInstanceOf[DatabaseNameProvider]
+
+    val name =
+      nameProvider.computeDatabaseName(schemaName, namespace)
+
+    DbMigrationConfig(
+      Database.databases(config.getString("database_kind")),
+      schemaName,
+      config.getString("host"),
+      config.getInt("port"),
+      config.getString("username"),
+      config.getString("password"),
+      nameProvider.computeDatabaseName(schemaName, namespace),
+      config.getString("create_database_statement").replace("@@DATABASE_NAME@@", name),
+      args.drop,
+      args.version,
+      config
+    )
+  }}
--- tool/src/main/scala/com/mnubo/dbschemas/DbSchemas.scala
+++ tool/src/main/scala/com/mnubo/dbschemas/DbSchemas.scala
@@ -6,49 +6,55 @@ import com.mnubo.app_util.MnuboConfiguration
 import com.typesafe.config.{Config, ConfigFactory}
 
 object DbSchemas extends App {
-  private val config = ConfigFactory
-    .defaultOverrides()
-    .withFallback(
-      MnuboConfiguration.loadConfig(
-        ConfigFactory
-          .parseFile(new File("db.conf"))
-          .withFallback(ConfigFactory.load())
+  private val config =
+    ConfigFactory
+      .defaultOverrides()
+      .withFallback(
+        MnuboConfiguration.loadConfig(
+          ConfigFactory
+            .parseFile(new File("db.conf"))
+            .withFallback(ConfigFactory.load())
+        )
       )
-    )
-  private val env = System.getenv("ENV")
+  private val env =
+    System.getenv("ENV")
   private val isSensitiveEnvironment =
     Set(MnuboConfiguration.Dev, MnuboConfiguration.Qa, MnuboConfiguration.Preprod, MnuboConfiguration.Prod).contains(env)
-  private val hasInstanceForEachNamespace = config.getBoolean("hasInstanceForEachNamespace")
+  private val hasInstanceForEachNamespace =
+    config.getBoolean("hasInstanceForEachNamespace")
+  private val schemaName =
+    config.getString("schema_name")
 
-  private val schemaName = config.getString("schema_name")
+  private val parser =
+    new scopt.OptionParser[DbSchemasArgsConfig](s"docker run -it --rm -e ENV=<environment name> dockerep-0.mtl.mnubo.com/$schemaName:latest") {
 
-  val parser = new scopt.OptionParser[DbSchemasArgsConfig](s"docker run -it --rm -e ENV=<environment name> dockerep-0.mtl.mnubo.com/$schemaName:latest") {
+      if (hasInstanceForEachNamespace)
+        head(s"Upgrades / downgrades the $schemaName database to the given version for all the namespaces.")
+      else
+        head(s"Upgrades / downgrades the $schemaName database to the given version.")
 
-    if (hasInstanceForEachNamespace)
-      head(s"Upgrades / downgrades the $schemaName database to the given version for all the namespaces.")
-    else
-      head(s"Upgrades / downgrades the $schemaName database to the given version.")
+      opt[String]('v', "version") action { (x, c) =>
+        c.copy(version = Some(x)) } text "The version you want to upgrade / downgrade to. If not specified, will upagrade to latest version."
 
-    opt[String]('v', "version") action { (x, c) =>
-      c.copy(version = Some(x)) } text "The version you want to upgrade / downgrade to. If not specified, will upagrade to latest version."
+      if (!isSensitiveEnvironment) {
+        opt[Unit]("drop") action { (_, c) =>
+          c.copy(drop = true)
+        } text "[DANGEROUS] Whether you want to first drop the database before migrating to the given version. WARNING! You will loose all your data, don't use this option in production!"
+      }
 
-    if (!isSensitiveEnvironment) {
-      opt[Unit]("drop") action { (_, c) =>
-        c.copy(drop = true)
-      } text "[DANGEROUS] Whether you want to first drop the database before migrating to the given version. WARNING! You will loose all your data, don't use this option in production!"
+      opt[Unit]("history") action { (_, c) =>
+        c.copy(cmd = DisplayHistory) } text "Display history of database migrations instead of migrating the database."
+
+      help("help") text "Display this schema manager usage."
+
+      note("Example:")
+
+      note(s"  docker run -it --rm -e ENV=dev dockerep-0.mtl.mnubo.com/$schemaName:latest --version=0004")
+
     }
 
-    opt[Unit]("history") action { (_, c) =>
-      c.copy(cmd = DisplayHistory) } text "Display history of database migrations instead of migrating the database."
-
-    help("help") text "Display this schema manager usage."
-
-    note("Example:")
-
-    note(s"  docker run -it --rm -e ENV=dev dockerep-0.mtl.mnubo.com/$schemaName:latest --version=0004")
-  }
-
   parser.parse(args, DbSchemasArgsConfig()).foreach { argConfig =>
+
     if (argConfig.drop && isSensitiveEnvironment)
       throw new Exception("Sorry, the --drop option is not available in dev, qa, preprod, or prod.")
 
@@ -59,7 +65,8 @@ object DbSchemas extends App {
         Seq(None)
 
     namespaces.foreach { ns =>
-      val cfg = buildConfig(argConfig, ns)
+      val cfg = DbMigrationConfig(argConfig, config, ns)
+
       argConfig.cmd match {
         case Migrate =>
           DatabaseMigrator.migrate(cfg)
@@ -68,47 +75,9 @@ object DbSchemas extends App {
       }
     }
   }
-
-  def buildConfig(args: DbSchemasArgsConfig, namespace: Option[String]) = {
-    val nameProvider = getClass.getClassLoader
-      .loadClass(config.getString("name_provider_class"))
-      .newInstance()
-      .asInstanceOf[DatabaseNameProvider]
-    val name = nameProvider.computeDatabaseName(schemaName, namespace)
-
-    DbMigrationConfig(
-      Database.databases(config.getString("database_kind")),
-      schemaName,
-      config.getString("host"),
-      config.getInt("port"),
-      config.getString("username"),
-      config.getString("password"),
-      nameProvider.computeDatabaseName(schemaName, namespace),
-      config.getString("create_database_statement").replace("@@DATABASE_NAME@@", name),
-      args.drop,
-      args.version,
-      config
-    )
-  }
-
-
 }
 
-case class DbSchemasArgsConfig(drop: Boolean = false, version: Option[String] = None, cmd: DbCommand = Migrate)
-
-case class DbMigrationConfig(db: Database,
-                             schemaName: String,
-                             host: String,
-                             port: Int,
-                             username: String,
-                             password: String,
-                             name: String,
-                             createDatabaseStatement: String,
-                             drop: Boolean,
-                             version: Option[String],
-                             wholeConfig: Config)
 
 sealed trait DbCommand
-
 case object Migrate extends DbCommand
 case object DisplayHistory extends DbCommand
