17b18257c1faff038d94c33cf47ce3494004da8d
Julien Letrouit <jletrouit@mnubo.com>
2015-08-05 16:49:13 -0400

    Bumped Mysql docker test image version to be able to connect through a localhost root user.

--- tool/src/main/scala/com/mnubo/dbschemas/MysqlDatabase.scala
+++ tool/src/main/scala/com/mnubo/dbschemas/MysqlDatabase.scala
@@ -35,7 +35,7 @@ object MysqlDatabase extends Database {
 
   override def testDockerBaseImage =
     DatabaseDockerImage(
-      name              = "dockerep-0.mtl.mnubo.com/test-mysql:5.6.24",
+      name              = "dockerep-0.mtl.mnubo.com/test-mysql:5.6.26",
       exposedPort       = 3306,
       isStarted         = (log, _) => log.contains("socket: '/var/run/mysqld/mysqld.sock'  port: 3306"),
       username          = "root",
